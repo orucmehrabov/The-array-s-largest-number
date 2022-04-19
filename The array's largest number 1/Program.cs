8348b878c77cb0d3dbec5383ebc37e48801ef593
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2022-04-19 18:03:06 +0400

    first commit

---
+++ The array's largest number 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace The_array_s_largest_number_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 18, 13, 6, 66, 35 };
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                    Console.Write("The largest pieces: " + arr[i]);
+                }
+            }
+        }
+    }
+}
